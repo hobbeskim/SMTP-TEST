165c613da69a90af49555e0f5b78913b907074fa
hobbeskim <hobbes1520@gmail.com>
2016-08-07 04:54:44 +0900

    adding source file test

---
+++ SMTP.py
@@ -0,0 +1,40 @@
+# -*- coding: cp949 -*-
+
+import mimetypes
+import smtplib
+
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+
+
+#global value
+
+host ="smtp.gmail.com"
+port="587"
+#htmlFilename=""
+
+senderAddr="bookinpeople@gmail.com"
+recipientAddr = "gowoonvv@hanmail.net"
+
+msg = MIMEBase("multipart","alternative")
+msg = MIMEText("this is auto mailing test.")
+msg['subject'] = "照括ぞぞぞぞぞぞぞぞぞぞ"
+msg['from'] = senderAddr
+msg['to'] = recipientAddr
+
+#create MIME document
+#htmlFD = open(htmlFileName, 'rb')
+#htmlpart = MIMEText(htmlFD,read(),'html'_charset = 'UTF-8')
+#htmlFD.close()
+
+#attach to MIMEBase
+#msg.attach(HtmlPart)
+
+#sending mail
+s = smtplib.SMTP(host,port)
+s.ehlo()
+s.starttls()
+s.ehlo()
+s.login("bookinpeople@gmail.com","book160218")
+s.sendmail(senderAddr, [recipientAddr], msg.as_string())
+s.close()
